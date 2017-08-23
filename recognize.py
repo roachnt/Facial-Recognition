fae68b01239adf8d4fd4a7f26a9953999d7af743
Nicklaus Roach <nicklausroach@Nicklauss-MacBook-Pro.local>
2017-08-22 20:13:21 -0400

    optimize

--- recognize.py
+++ recognize.py
@@ -7,6 +7,7 @@ import face_recognition
 # Python Image Library
 from PIL import Image
 
+cv2.useOptimized()
 # Get haar cascade file
 cascPath = "haarcascades/haarcascade_frontalface_default.xml"
 
@@ -40,22 +41,20 @@ while True:
         flags=cv2.CASCADE_SCALE_IMAGE
     )
 
-
-    if face_recognition.face_encodings(frame):
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            roi = frame[y:y+h, x:x+w]
-            if face_recognition.face_encodings(roi):
-                unknown_face_encoding = face_recognition.face_encodings(roi)[0]
-                results_me = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
-                if results_me[0]:
-                    cv2.putText(frame, 'Nick Roach', (x - 1, y - 1), cv2.FONT_HERSHEY_TRIPLEX,2,(0, 0, 255))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        roi = frame[y:y+h, x:x+w]
+        if face_recognition.face_encodings(roi):
+            unknown_face_encoding = face_recognition.face_encodings(roi)[0]
+            results_me = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
+            if results_me[0]:
+                cv2.putText(frame, 'Nick Roach', (x - 1, y - 1), cv2.FONT_HERSHEY_TRIPLEX,2,(0, 0, 255))
         # Display the resulting frame
-        cv2.imshow('Facial Recognition Application', frame)
+    cv2.imshow('Facial Recognition Application', frame)
 
                 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # When everything is done, release the capture
 video_capture.release()
