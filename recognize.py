e016cf782b4050f066ca9aa8e03c6b159e01c5f5
Nick Roach <roachnt@miamioh.edu>
2017-08-22 20:28:47 -0400

    comment, refactor

--- recognize.py
+++ recognize.py
@@ -7,7 +7,9 @@ import face_recognition
 # Python Image Library
 from PIL import Image
 
+# Optimize
 cv2.useOptimized()
+
 # Get haar cascade file
 cascPath = "haarcascades/haarcascade_frontalface_default.xml"
 
@@ -41,15 +43,31 @@ while True:
         flags=cv2.CASCADE_SCALE_IMAGE
     )
 
+    # Iterate through each face in the frame
     for (x, y, w, h) in faces:
+
+        # Draw a rectangle around the face
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # Get the rectangle of interest
         roi = frame[y:y+h, x:x+w]
+
+        # If a face is found in the rectangle
         if face_recognition.face_encodings(roi):
+
+            # Get the face encoding
             unknown_face_encoding = face_recognition.face_encodings(roi)[0]
-            results_me = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
-            if results_me[0]:
+
+            # Get result of comparison between my face and face in rectangle of interest
+            results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
+
+            # If the two are a match
+            if results[0]:
+
+                # Put my name above the rectangle
                 cv2.putText(frame, 'Nick Roach', (x - 1, y - 1), cv2.FONT_HERSHEY_TRIPLEX,2,(0, 0, 255))
-        # Display the resulting frame
+                
+    # Display the resulting frame
     cv2.imshow('Facial Recognition Application', frame)
 
                 
